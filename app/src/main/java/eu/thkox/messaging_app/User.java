b25b44f5cfdbcbf362cfa1334116853028478520
thkox <theokoxan@gmail.com>
2024-01-03 19:06:52 +0200

    create user class and ChatsActivity

---
+++ app/src/main/java/eu/thkox/messaging_app/User.java
@@ -0,0 +1,14 @@
+package eu.thkox.messaging_app;
+
+public class User {
+
+    String email;
+    String nickname;
+    String id;
+
+    public User(String email, String nickname, String id) {
+        this.email = email;
+        this.nickname = nickname;
+        this.id = id;
+    }
+}
