54ff90aff2cf1c6255b752f56f29698e4211a241
thkox <theokoxan@gmail.com>
2024-01-03 19:24:09 +0200

    configure the layout of the activity_chats.xml

--- app/src/main/java/eu/thkox/messaging_app/User.java
+++ app/src/main/java/eu/thkox/messaging_app/User.java
@@ -6,6 +6,30 @@ public class User {
     String nickname;
     String id;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public User(String email, String nickname, String id) {
         this.email = email;
         this.nickname = nickname;
