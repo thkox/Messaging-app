19fefb22adace23ca0c013f50189b4f304396a1a
thkox <theokoxan@gmail.com>
2024-01-04 19:28:05 +0200

    create auto login/sign-in for the app user

--- app/src/main/java/eu/thkox/messaging_app/MainActivity.java
+++ app/src/main/java/eu/thkox/messaging_app/MainActivity.java
@@ -7,15 +7,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     Button buttonLogIn;
     Button buttonRegister;
 
+    FirebaseUser firebaseUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null) {
+            Intent intent = new Intent(this, ChatsActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         buttonLogIn = findViewById(R.id.buttonLogIn);
         buttonRegister = findViewById(R.id.buttonRegister);
