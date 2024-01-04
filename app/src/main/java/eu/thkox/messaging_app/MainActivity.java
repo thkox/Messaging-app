7e65b0820258cf5f9d5001ad5e5e0d9e77cede4d
thkox <theokoxan@gmail.com>
2024-01-04 16:35:54 +0200

    complete sign-up connection with Firebase

--- app/src/main/java/eu/thkox/messaging_app/MainActivity.java
+++ app/src/main/java/eu/thkox/messaging_app/MainActivity.java
@@ -3,12 +3,19 @@ package eu.thkox.messaging_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToSignInActivity(View view) {
+
     }
 }
--- app/src/main/java/eu/thkox/messaging_app/SignUpActivity.java
+++ app/src/main/java/eu/thkox/messaging_app/SignUpActivity.java
@@ -1,14 +1,102 @@
 package eu.thkox.messaging_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText editTextNickname, editTextEmail, editTextPassword;
+
+    TextView textViewSignUpError;
+    Button buttonSignUp;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editTextNickname = findViewById(R.id.editTextSignUpTextNickname);
+        editTextEmail = findViewById(R.id.editTextSignUpTextEmailAddress);
+        editTextPassword = findViewById(R.id.editTextSignUpTextPassword);
+        textViewSignUpError = findViewById(R.id.textViewSignUpError);
+
+        textViewSignUpError.setVisibility(TextView.INVISIBLE);
+
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+
+        auth = FirebaseAuth.getInstance();
     }
+
+    public void signUpUser(View view) {
+        String nickname = editTextNickname.getText().toString();
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        if (TextUtils.isEmpty(nickname) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            textViewSignUpError.setVisibility(TextView.VISIBLE);
+            textViewSignUpError.setText(R.string.all_fields_are_required);
+        } else if (password.length() < 4) {
+            textViewSignUpError.setVisibility(TextView.VISIBLE);
+            textViewSignUpError.setText(R.string.password_must_be_at_least_4_characters_long);
+        } else {
+            registerUser(nickname, email, password);
+        }
+    }
+
+    private void registerUser(String nickname, String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser firebaseUser = auth.getCurrentUser();
+                    assert firebaseUser != null;
+                    String userid = firebaseUser.getUid();
+
+                    reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    hashMap.put("id", userid);
+                    hashMap.put("nickname", nickname);
+                    hashMap.put("imageURL", "default");
+
+                    reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Intent intent = new Intent(SignUpActivity.this, ChatsActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                    });
+                }else{
+                    textViewSignUpError.setVisibility(TextView.VISIBLE);
+                    textViewSignUpError.setText(R.string.you_can_t_register_with_this_email_or_password);
+                }
+            }
+        });
+    }
+
 }
